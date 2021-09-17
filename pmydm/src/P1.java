6e80bc6742c0576415aabde8e06bcfe8a57f7623
ululu91 <juanlumo91@gmail.com>
2021-09-17 16:51:20 +0200

    Primera version

--- pmydm/src/P1.java
+++ pmydm/src/P1.java
@@ -1,3 +1,30 @@
 public class P1 {
-    hola
+    public class main3 {
+        public static void main(String[] args)
+        {
+
+            String sNombre ="alb erto fer alf al7 dez dez san chez";
+
+            //imprimir lista de palabras de un texto
+
+
+            // imprimir la lista de palabras de tamaño impar de un texto ( con split)
+
+
+
+        String arraySeccion [] = sNombre.split(" ");
+
+        for(int i = 0; i < arraySeccion.length ;i++ )
+        {
+            if(arraySeccion[i].length()% 2 != 0)
+            {
+                System.out.println(arraySeccion[i]);
+            }
+
+        }
+            // numero de palabras iguales
+            // sustituir las vocales por un 2 de las palabras que empiecen por "el"
+        }
+    }
+
 }
